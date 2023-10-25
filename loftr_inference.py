ffa85768716ccd3a8ff1dd01a0d7333cfab72cc7
Avichai <avichai@airis-labs.com>
2023-10-25 11:07:57 +0000

    More clean code

--- loftr_inference.py
+++ loftr_inference.py
@@ -1,6 +1,6 @@
-from third_party.experiments.matching import *
-from third_party.experiments.preprocessing import *
-from third_party.experiments.drawing import *
+from experiments.matching import *
+from experiments.preprocessing import *
+from experiments.drawing import *
 
 def run_inference(img0: np.ndarray, img1: np.ndarray, weights_path: str = None, is_indoor: bool = False, show_matches: bool = False, conf_factor: float = 0.0, save_path: str = None):
     # initiate LoFTR model
